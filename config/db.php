f6e76dd8390716748f0d19cbb0ab379866dba7df
Bram den Ouden <bramdenouden96@hotmail.com>
2020-10-12 14:57:37 +0200

    docker setup

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=workshop',
+    'dsn' => 'mysql:host=bram-ubuntu.local;dbname=workshop',
     'username' => 'root',
-    'password' => '',
+    'password' => 'verysecret',
     'charset' => 'utf8',
 ];
